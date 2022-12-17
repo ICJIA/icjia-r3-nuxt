642de46b9b1ffeb4a4f05e7b4f1d4181eeeb8b17
cschweda <cschweda@gmail.com>
2022-12-17 11:59:01 -0600

    fix: stickify TOC

--- pages/news/[...slug].vue
+++ pages/news/[...slug].vue
@@ -27,41 +27,103 @@ onMounted(() => {
 
 <template>
   <div>
-    <v-container v-if="data" class="mb-12 mt-6" fluid
-      ><v-row
-        ><v-col cols="12" :md="cols">
-          <div>
-            <h1>{{ data.title }}</h1>
-            <ContentDoc :key="data?.title" :value="data" class="markdown-body">
-              <template #empty>Document not found</template>
-              <template #not-found>Document not found</template>
-            </ContentDoc>
-          </div>
-          <!-- <div v-else>{{ redirect() }}</div> -->
-        </v-col>
+    <div v-if="data">
+      <v-container class="mb-12 mt-6" fluid
+        ><v-row
+          ><v-col cols="12" :md="cols">
+            <div>
+              <h1>{{ data.title }}</h1>
+              <ContentDoc
+                :key="data?.title"
+                :value="data"
+                class="markdown-body"
+              >
+                <template #empty>Document not found</template>
+                <template #not-found>Document not found</template>
+              </ContentDoc>
+            </div>
+            <!-- <div v-else>{{ redirect() }}</div> -->
+          </v-col>
 
-        <v-col
-          v-if="showTOC"
-          cols="12"
-          md="4"
-          style="min-height: 40vh !important"
-          class="hidden-sm-and-down"
-        >
-          <v-card
-            elevation="0"
-            class="px-5 py-5 markdown-body"
-            style="background: #fafafa; margin-top: 85px"
+          <v-col
+            v-if="showTOC"
+            cols="12"
+            md="4"
+            style="min-height: 40vh !important"
+            class="hidden-sm-and-down"
           >
-            <h2>TODO: Table of Contents</h2>
-
-            <strong>Show table of contents: </strong>
-            {{ data.showTableOfContents }}<br /><br /><strong
-              >Table of Contents</strong
+            <v-card
+              elevation="0"
+              class="toc px-5 py-5 markdown-body"
+              style="background: #fafafa"
             >
-            {{ data.body.toc }}
-          </v-card>
-        </v-col>
-      </v-row></v-container
-    >
+              <h2>TODO: Table of Contents</h2>
+
+              <strong>Show table of contents: <br /></strong>
+              {{ data.showTableOfContents }}<br /><br /><strong
+                >Table of Contents: <br />
+              </strong>
+              {{ data.body.toc }}
+            </v-card>
+          </v-col>
+        </v-row></v-container
+      >
+    </div>
   </div>
 </template>
+
+<style scoped>
+.divider {
+  border-left: 1px solid #ccc;
+}
+.visible {
+  color: #0d4474;
+  font-weight: bold;
+}
+.anchor {
+  padding: 2px 5px 2px 5px;
+}
+.anchor:hover {
+  color: #0d4474;
+  background: #eee;
+}
+.shaded {
+  background: #eee;
+  padding: 15px;
+  margin-bottom: 25px;
+}
+.toc {
+  position: -webkit-sticky !important;
+  position: sticky !important;
+  top: 185px !important;
+  font-size: 14px;
+}
+ul.toc-list {
+  list-style-type: none;
+}
+ul.toc-list li {
+  color: #333;
+  padding: 2px 5px 2px 5px;
+  cursor: pointer;
+  margin-bottom: 3px;
+  font-size: 14px;
+}
+ul.toc-list li:hover {
+  color: #aaa;
+  background: #eee;
+}
+.tocListItem {
+  margin-left: -20px !important;
+}
+/* .tocItem {
+} */
+@media only screen and (max-width: 1024px) {
+  /* .divider {
+    border-left: 0px solid #ccc;
+  } */
+  ul.toc-list li:hover {
+    color: #fff;
+    background: #aaa;
+  }
+}
+</style>
