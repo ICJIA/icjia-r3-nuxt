59c4d372854c68f420c6b29376cad5ed316b2572
cschweda <cschweda@gmail.com>
2022-12-17 10:52:06 -0600

    feat: scaffold news listing

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,32 +1,65 @@
 <template>
-  <div>
-    <h1>All news posts</h1>
-    <div v-if="data">
-      {{ data }}
-    </div>
-    <!-- <nuxtLink to="/news/test-post-1">Test Post 1</nuxtLink><br />
-    <nuxtLink to="/news/test-post-2">Test Post 2</nuxtLink><br />
-    <nuxtLink to="/news/test-post-3">Test Post 3</nuxtLink><br /> -->
+  <div class="mb-10">
+    <v-container
+      ><v-row
+        ><v-col cols="12">
+          <h1>News and Updates</h1>
+
+          <div v-if="news" class="mt-12">
+            <v-card
+              v-for="(item, index) in sortedNews"
+              :key="index"
+              color="grey-lighten-4"
+              class="px-5 py-10 my-10 info-card"
+              @click="routeToNews(item.path)"
+            >
+              <div style="font-size: 10px; font-weight: 900">
+                {{ formatDate(item.createdAt) }}
+              </div>
+              <h2 class="mt-5">{{ item.title }}</h2>
+              <div class="mt-8">{{ item.summary }}</div>
+            </v-card>
+          </div>
+        </v-col></v-row
+      ></v-container
+    >
   </div>
 </template>
 
 <script setup>
+import _ from "lodash";
 // definePageMeta({
 //   middleware: ["is-valid-content"],
 // });
 useHead({
-  title: "Latest News",
+  title: "News and Updates",
 });
 const { path } = useRoute();
 const router = useRouter();
 
 // const error = useError();
-const { data } = await useAsyncData(`news-${path}`, async () => {
+const { data: news } = await useAsyncData(`news-${path}`, async () => {
   const post = await queryContent("/news").find();
   return post;
 });
+
+const sortedNews = _.orderBy(news.value, ["createdAt"], ["desc"]);
+
 const redirect = () => {
   router.push("/404");
+};
+
+const routeToNews = (path) => {
+  router.push(path);
+};
+
+const formatDate = (date) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return new Date(date).toLocaleDateString("en-US", options);
 };
 </script>
 
